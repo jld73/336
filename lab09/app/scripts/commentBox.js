8146c1fb20528b47d94f4ca5af667572a3f99d02
Jordan Doorlag <jld73@students.calvin.edu>
2018-11-02 13:26:05 -0400

    Lab09 submission

---
+++ lab09/app/scripts/commentBox.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Remarkable from 'remarkable';
+import $ from 'jquery';
+
+import CommentForm from './commentForm.js'
+import CommentList from './commentList.js'
+
+module.exports = React.createClass({
+    getInitialState: function () {
+        return { data: [] };
+    },
+    loadCommentsFromServer: function () {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({ data: data });
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleCommentSubmit: function (comment) {
+        var comments = this.state.data;
+        // Optimistically set id 
+        comment.id = Date.now();
+        var newComments = comments.concat([comment]);
+        this.setState({ data: newComments });
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function (data) {
+                this.setState({ data: data });
+            }.bind(this),
+            error: function (xhr, status, err) {
+                this.setState({data: comments});
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    componentDidMount: function () {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments Box test</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+            </div>
+        );
+    }
+});
